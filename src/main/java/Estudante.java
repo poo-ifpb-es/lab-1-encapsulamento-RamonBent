d6bd1c63dfeed27f364bc32285249fec55972a37
RamonBent <ramon.bento.oliver@gmail.com>
2023-09-04 21:40:45 -0300

    Cria classe Estudante

---
+++ src/main/java/Estudante.java
@@ -0,0 +1,23 @@
+package main.java;
+
+public class Estudante {
+
+    private String nome;
+    private int matricula;
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getMatricula() {
+        return this.matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+}
