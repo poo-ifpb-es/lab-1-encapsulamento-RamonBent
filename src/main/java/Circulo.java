28f71a6acbc9a506847d606c97cf0aa26daf0497
RamonBent <ramon.bento.oliver@gmail.com>
2023-09-10 14:37:08 -0300

    Cria classe Circulo

--- src/main/java/Circulo.java
+++ src/main/java/Circulo.java
@@ -1,25 +1,28 @@
-//package br.edu.ifpb.lab1;
-//
-//public class Circulo {
-//    private float raio;
-//    private float area;
-//    private float circunferencia;
-//
-//    Circulo (float raio){
-//        this.raio = raio;
-//    }
-//
-//    public float getRaio(){
-//        return this.raio;
-//
-//    }
-//    public void setRaio(float raio){
-//        this.raio = raio;
-//    }
-//    public float getArea(){
-//        return this.area;
-//    }
-//    public float getCircunferencia(){
-//
-//    }
-//}
+public class Circulo {
+
+    private static final float PI = 3.14159f;
+
+    private float raio;
+    private float area;
+    float circunferencia;
+
+    public Circulo(float raio) {
+        this.raio = raio;
+    }
+
+    public float getRaio() {
+        return this.raio;
+    }
+
+    public void setRaio(float raio) {
+        this.raio = raio;
+    }
+
+    public float getArea() {
+        return (float) (PI * Math.pow(raio, 2));
+    }
+
+    public float getCircunferencia() {
+        return 2 * PI * raio;
+    }
+}
