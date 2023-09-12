00a1eb458219c18cdcfe1d2aee94bdda631dd4b6
RamonBent <ramon.bento.oliver@gmail.com>
2023-09-11 21:50:05 -0300

    Cria classes Ponto e Linha

---
+++ src/main/java/Ponto.java
@@ -0,0 +1,29 @@
+public class Ponto {
+    public double x;
+    public double y;
+
+    public Ponto(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return this.x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return this.y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+    public void setXY(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+}
